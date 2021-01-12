0bc277e99ac96815daede6fd8e8efe37e5d80d9b
tangjianfeng <tangjianfeng@egova.com.cn>
2021-01-12 09:42:25 +0800

    [M]update README.md and deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd ./dist
+cd docs/.vuepress/dist
 
 
 git init
@@ -18,5 +18,4 @@ git commit -m 'deploy'
 # 把下面的push命令按照你的情况修改后去掉注释
 # 如果发布到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f https://github.com/elrictang/elrictang.github.io master
